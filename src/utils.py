de54fb4defdf3abfa3986d6e6889915e035adcde
= <=>
2025-02-19 10:12:03 +0530

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import pickle
 
  
  
@@ -52,5 +53,21 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+# def load_object(file_path):
+#     try:
+#         with open(file_path, "rb") as file_obj:
+#             return pickle.load(file_obj)
+
+#     except Exception as e:
+#         raise CustomException(e, sys)
+ 
+
+def load_object(file_path):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Preprocessor file not found at {file_path}. Ensure it is generated.")
+    with open(file_path, "rb") as file_obj:
+        return pickle.load(file_obj)
+
  
     
